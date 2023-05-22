fb66d47c0e6f348214fdbcc2c87300f6bd79273a
NB <nash4comp@gmail.com>
2023-05-21 21:17:03 -0700

    Setup 7 problems to solve

--- easy/2023-05-20-Meeting-Rooms.py
+++ easy/2023-05-20-Meeting-Rooms.py
@@ -1,39 +1,55 @@
-# https://www.lintcode.com/problem/920/
+# # https://www.lintcode.com/problem/920/
 
-class Interval:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
+# class Interval:
+#     def __init__(self, start, end):
+#         self.start = start
+#         self.end = end
 
-class Solution:
-    def can_attend_meetings(self, intervals: list[Interval]) -> bool:
-        intervals.sort(key = lambda x: x.start)
+# class Solution:
+#     def can_attend_meetings(self, intervals: list[Interval]) -> bool:
+#         intervals.sort(key = lambda x: x.start)
 
-        for i in range(1, len(intervals)):
-            temp1 = intervals[i - 1]
-            temp2 = intervals[i]
+#         for i in range(1, len(intervals)):
+#             temp1 = intervals[i - 1]
+#             temp2 = intervals[i]
 
-            if temp1.end > temp2.start:
-                return False
-        return True
+#             if temp1.end > temp2.start:
+#                 return False
+#         return True
     
-interval1 = Interval(0, 30)
-interval2 = Interval(5, 10)
-interval3 = Interval(15, 20)
+# interval1 = Interval(0, 30)
+# interval2 = Interval(5, 10)
+# interval3 = Interval(15, 20)
 
-interval4 = Interval(5, 8)
-interval5= Interval(9, 15)
+# interval4 = Interval(5, 8)
+# interval5= Interval(9, 15)
 
-intervals1 = []
-intervals1.append(interval1)
-intervals1.append(interval2)
-intervals1.append(interval3)
+# intervals1 = []
+# intervals1.append(interval1)
+# intervals1.append(interval2)
+# intervals1.append(interval3)
 
-intervals2 = []
-intervals2.append(interval4)
-intervals2.append(interval5)
+# intervals2 = []
+# intervals2.append(interval4)
+# intervals2.append(interval5)
 
-solution = Solution()
-print(solution.can_attend_meetings(intervals1))
-print(solution.can_attend_meetings(intervals2))
+# solution = Solution()
+# print(solution.can_attend_meetings(intervals1))
+# print(solution.can_attend_meetings(intervals2))
+
+def main():
+    operations = {
+        "add": lambda x, y: x + y,
+        "subtract": lambda x, y: x - y,
+        "multiply": lambda x, y: x * y,
+        "divide": lambda x, y: x / y
+    }
+
+    result = operations["subtract"](3, 4)
+    print(result)
+
+
+
+if __name__ == '__main__':
+    main()
 
